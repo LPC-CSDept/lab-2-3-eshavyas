768526b837e0222f9da7d792a233b070d2f3db10
eshavyas <vyase58@gmail.com>
2023-02-02 17:19:53 -0800

    Lab 2-3

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    var1 = int(input('Enter your first number'))
+    var2 = int(input('Enter your second number '))
+    var3 = int(input('Enter your third number '))
 
-    pass
+    total = var1 + var2 + var3
+    print(f'The summation is {total:>20}')
+
+    avg = total / 3
+    print(f'The summation is {avg:.2f}')    
+    # pass
 
 
 if __name__ == '__main__':
